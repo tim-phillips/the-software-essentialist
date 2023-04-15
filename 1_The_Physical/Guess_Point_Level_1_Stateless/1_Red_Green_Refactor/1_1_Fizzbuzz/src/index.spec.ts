e81b3d89ba69e9018c360f5f7e9c56ee316bf788
Tim Phillips <timphillips@hey.com>
2023-04-14 22:35:18 -0700

    feat(fizzbuzz): validate input

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,5 +1,22 @@
+import { fizzbuzz } from "./fizzbuzz";
 
-describe("fizzbuzz", () => {
+// Write a function that takes numbers from 1 to 100
+// and outputs them as a string,
+// but for multiples of three it returns “Fizz”
+// and for multiples of five it returns “Buzz”
+// For multiples of both three and five, it returns “FizzBuzz”
 
-
+describe("FizzBuzz", () => {
+  test("takes numbers from 1 to 100", () => {
+    expect(() => fizzbuzz(-23)).toThrow();
+    expect(() => fizzbuzz(-1)).toThrow();
+    expect(() => fizzbuzz(0)).toThrow();
+    expect(() => fizzbuzz(1)).not.toThrow();
+    expect(() => fizzbuzz(11)).not.toThrow();
+    expect(() => fizzbuzz(54)).not.toThrow();
+    expect(() => fizzbuzz(87)).not.toThrow();
+    expect(() => fizzbuzz(100)).not.toThrow();
+    expect(() => fizzbuzz(101)).toThrow();
+    expect(() => fizzbuzz(131)).toThrow();
+  });
 });
