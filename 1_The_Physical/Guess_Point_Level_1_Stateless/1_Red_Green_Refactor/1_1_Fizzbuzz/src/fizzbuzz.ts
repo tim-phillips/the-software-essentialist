1f9eab544cae2dd842cddbe5473a1624038d28c4
Tim Phillips <timphillips@hey.com>
2023-04-14 22:45:57 -0700

    feat(fizzbuzz): output Fizz for multiples of three

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -2,5 +2,8 @@ export function fizzbuzz(n: number) {
   if (n < 1 || n > 100) {
     throw Error("Must use a number between 1 and 100");
   }
+  if (n % 3 === 0) {
+    return "Fizz";
+  }
   return n.toString();
 }
