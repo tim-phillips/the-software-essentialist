6597d2c8d95752349c592ec310a6e06eca33446e
Tim Phillips <timphillips@hey.com>
2023-05-07 15:58:55 -0700

    test(palindrome): racecar

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/isPalindrome.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/isPalindrome.spec.ts
@@ -1,9 +1,12 @@
 import { isPalindrome } from "./isPalindrome";
 
 describe(isPalindrome.name, () => {
-  it.each(["mom", "wow"])("knows that '%s' is a palindrome", (word) => {
-    expect(isPalindrome(word)).toBeTruthy();
-  });
+  it.each(["mom", "wow", "racecar"])(
+    "knows that '%s' is a palindrome",
+    (word) => {
+      expect(isPalindrome(word)).toBeTruthy();
+    }
+  );
 
   it("knows that 'bill' is not a palindrome", () => {
     expect(isPalindrome("bill")).toBeFalsy();
