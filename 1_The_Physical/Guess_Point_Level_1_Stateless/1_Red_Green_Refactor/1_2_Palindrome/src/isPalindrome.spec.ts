95703c3f70c9fca13f67d026f70398dc1dae6b79
Tim Phillips <timphillips@hey.com>
2023-05-07 20:19:22 -0700

    test(palindrome): spaces

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/isPalindrome.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/isPalindrome.spec.ts
@@ -1,12 +1,16 @@
 import { isPalindrome } from "./isPalindrome";
 
 describe(isPalindrome.name, () => {
-  it.each(["mom", "wow", "racecar", "Mom", "Was It A Rat I Saw"])(
-    "knows that '%s' is a palindrome",
-    (word) => {
-      expect(isPalindrome(word)).toBeTruthy();
-    }
-  );
+  it.each([
+    "mom",
+    "wow",
+    "racecar",
+    "Mom",
+    "Was It A Rat I Saw",
+    "Never Odd or Even",
+  ])("knows that '%s' is a palindrome", (word) => {
+    expect(isPalindrome(word)).toBeTruthy();
+  });
 
   it.each(["bill", "tomato"])("knows that '%s' is not a palindrome", (word) => {
     expect(isPalindrome(word)).toBeFalsy();
