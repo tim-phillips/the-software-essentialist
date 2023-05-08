3b4baa86fa555c5fa7743a39f7d3c5e4d1638753
Tim Phillips <timphillips@hey.com>
2023-05-07 20:21:41 -0700

    refactor(palindrome): create remove spaces function

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/isPalindrome.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/isPalindrome.ts
@@ -1,10 +1,14 @@
 export function isPalindrome(word: string): boolean {
   return (
-    reverseWord(word.replace(/ /g, "")).toLowerCase() ===
-    word.replace(/ /g, "").toLowerCase()
+    reverseWord(removeSpaces(word)).toLowerCase() ===
+    removeSpaces(word).toLowerCase()
   );
 }
 
 function reverseWord(word: string): string {
   return word.split("").reverse().join("");
+}
+
+function removeSpaces(word: string): string {
+  return word.replace(/ /g, "");
 }
