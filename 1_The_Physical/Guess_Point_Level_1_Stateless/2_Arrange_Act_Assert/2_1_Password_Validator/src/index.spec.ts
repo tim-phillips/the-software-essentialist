e2413ca9537e5cb3d87567624dbafafb8678a992
Tim Phillips <timphillips@hey.com>
2023-05-12 11:20:37 -0700

    feat(password): return error for long password

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
@@ -8,4 +8,12 @@ describe(validatePassword.name, () => {
     expect(response.errors.length).toEqual(1);
     expect(response.errors[0]).toEqual("InvalidLength");
   });
+
+  it("returns error for long password", () => {
+    const response = validatePassword("suchlengthsuchwow");
+
+    expect(response.result).toEqual(false);
+    expect(response.errors.length).toEqual(1);
+    expect(response.errors[0]).toEqual("InvalidLength");
+  });
 });
