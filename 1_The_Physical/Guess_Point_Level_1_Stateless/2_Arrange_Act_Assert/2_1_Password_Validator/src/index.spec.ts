c1eda85b4eac3d8f2e66c17f3e1b5d5585acd355
Tim Phillips <timphillips@hey.com>
2023-05-12 11:19:10 -0700

    feat(password): return error for short password

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
@@ -1,5 +1,11 @@
+import { validatePassword } from "./";
 
-describe('password validator', () => {
+describe(validatePassword.name, () => {
+  it("returns error for short password", () => {
+    const response = validatePassword("tiny");
 
-})
-
+    expect(response.result).toEqual(false);
+    expect(response.errors.length).toEqual(1);
+    expect(response.errors[0]).toEqual("InvalidLength");
+  });
+});
