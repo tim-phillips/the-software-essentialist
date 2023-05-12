7f8f61f580b7e1da97d51da1f112aefee8fdbe8b
Tim Phillips <timphillips@hey.com>
2023-05-12 11:33:32 -0700

    feat(password): return error for password without a digit

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
@@ -2,14 +2,14 @@ import { validatePassword } from "./";
 
 describe(validatePassword.name, () => {
   it("returns true for password between 5 and 15 characters", () => {
-    const response = validatePassword("agoodpassword");
+    const response = validatePassword("agoodpassw0rd");
 
     expect(response.result).toEqual(true);
     expect(response.errors.length).toEqual(0);
   });
 
   it("returns error for short password", () => {
-    const response = validatePassword("tiny");
+    const response = validatePassword("t1ny");
 
     expect(response.result).toEqual(false);
     expect(response.errors.length).toEqual(1);
@@ -17,10 +17,18 @@ describe(validatePassword.name, () => {
   });
 
   it("returns error for long password", () => {
-    const response = validatePassword("suchlengthsuchwow");
+    const response = validatePassword("suchl3ngthsuchwow");
 
     expect(response.result).toEqual(false);
     expect(response.errors.length).toEqual(1);
     expect(response.errors[0]).toEqual("InvalidLength");
+  });
+
+  it("returns error for password without a digit", () => {
+    const response = validatePassword("apassnodigit");
+
+    expect(response.result).toEqual(false);
+    expect(response.errors.length).toEqual(1);
+    expect(response.errors[0]).toEqual("MissingDigit");
   });
 });
