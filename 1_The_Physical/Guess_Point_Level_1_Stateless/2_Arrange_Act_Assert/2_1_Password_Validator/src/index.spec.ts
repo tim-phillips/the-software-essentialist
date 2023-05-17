994fefcae46eee79e5905ef7ff8bc8415518e47d
Tim Phillips <timphillips@hey.com>
2023-05-16 19:03:38 -0700

    test(password): use it.each for false cases

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
@@ -5,87 +5,31 @@ describe(validatePassword.name, () => {
     "validates '%s' is a valid password",
     (password) => {
       const response = validatePassword(password);
-
       expect(response.result).toEqual(true);
       expect(response.errors.length).toEqual(0);
     }
   );
 
-  it("returns error for short password containing a digit and an upper case letter", () => {
-    const response = validatePassword("t1Ny");
-
+  it.each([
+    ["t1Ny", ["InvalidLength"]],
+    ["suchl3ngthsuchwoW", ["InvalidLength"]],
+    ["apassNodigit", ["MissingDigit"]],
+    ["apassn0upper", ["MissingUppercase"]],
+    ["apass", ["MissingDigit", "MissingUppercase"]],
+    ["t1ny", ["InvalidLength", "MissingUppercase"]],
+    ["t1ny", ["InvalidLength", "MissingUppercase"]],
+    ["suchl3ngthsuchwow", ["InvalidLength", "MissingUppercase"]],
+    ["suchlEngthsuchwow", ["InvalidLength", "MissingDigit"]],
+    [
+      "suchlengthsuchwow",
+      ["InvalidLength", "MissingUppercase", "MissingDigit"],
+    ],
+  ])("returns error for '%s'", (password, errors) => {
+    const response = validatePassword(password);
     expect(response.result).toEqual(false);
-    expect(response.errors.length).toEqual(1);
-    expect(response.errors).toContain("InvalidLength");
-  });
-
-  it("returns error for long password containing a digit and an upper case letter", () => {
-    const response = validatePassword("suchl3ngthsuchwoW");
-
-    expect(response.result).toEqual(false);
-    expect(response.errors.length).toEqual(1);
-    expect(response.errors).toContain("InvalidLength");
-  });
-
-  it("returns error for password between 5 and 15 characters with an upper case letter without a digit", () => {
-    const response = validatePassword("apassNodigit");
-
-    expect(response.result).toEqual(false);
-    expect(response.errors.length).toEqual(1);
-    expect(response.errors).toContain("MissingDigit");
-  });
-
-  it("returns error for password between 5 and 15 characters with a digit without an upper case letter", () => {
-    const response = validatePassword("apassn0upper");
-
-    expect(response.result).toEqual(false);
-    expect(response.errors.length).toEqual(1);
-    expect(response.errors).toContain("MissingUppercase");
-  });
-
-  it("returns errors for password between 5 and 15 characters without a digit or an upper case letter", () => {
-    const response = validatePassword("apass");
-
-    expect(response.result).toEqual(false);
-    expect(response.errors.length).toEqual(2);
-    expect(response.errors).toContain("MissingDigit");
-    expect(response.errors).toContain("MissingUppercase");
-  });
-
-  it("returns errors for short password with a digit without an upper case letter", () => {
-    const response = validatePassword("t1ny");
-
-    expect(response.result).toEqual(false);
-    expect(response.errors.length).toEqual(2);
-    expect(response.errors).toContain("InvalidLength");
-    expect(response.errors).toContain("MissingUppercase");
-  });
-
-  it("returns errors for long password with a digit without an upper case letter", () => {
-    const response = validatePassword("suchl3ngthsuchwow");
-
-    expect(response.result).toEqual(false);
-    expect(response.errors.length).toEqual(2);
-    expect(response.errors).toContain("InvalidLength");
-    expect(response.errors).toContain("MissingUppercase");
-  });
-
-  it("returns errors for long password with an upper case letter without a digit", () => {
-    const response = validatePassword("suchlEngthsuchwow");
-
-    expect(response.result).toEqual(false);
-    expect(response.errors.length).toEqual(2);
-    expect(response.errors).toContain("InvalidLength");
-    expect(response.errors).toContain("MissingDigit");
-  });
-
-  it("returns errors for long password without a digit or an upper case letter", () => {
-    const response = validatePassword("suchlengthsuchwow");
-
-    expect(response.result).toEqual(false);
-    expect(response.errors.length).toEqual(3);
-    expect(response.errors).toContain("InvalidLength");
-    expect(response.errors).toContain("MissingUppercase");
-    expect(response.errors).toContain("MissingDigit");
+    expect(response.errors.length).toEqual(errors.length);
+    for (const error of errors) {
+      expect(response.errors).toContain(error);
+    }
   });
 });
