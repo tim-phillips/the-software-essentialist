a6be62a808fbef24eab3823f8e3475bcb14103ff
Tim Phillips <timphillips@hey.com>
2023-05-18 19:37:34 -0700

    feat(stats): return min -8

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
@@ -1,5 +1,9 @@
+import { statsCalculator } from "./";
 
-describe('stats calculator', () => {
-
-
-})
+describe(statsCalculator.name, () => {
+  it("knows that -8 is the minimum value", () => {
+    const seq = [2, 4, 21, -8, 53, 40];
+    const response = statsCalculator(seq);
+    expect(response.min).toBe(-8);
+  });
+});
