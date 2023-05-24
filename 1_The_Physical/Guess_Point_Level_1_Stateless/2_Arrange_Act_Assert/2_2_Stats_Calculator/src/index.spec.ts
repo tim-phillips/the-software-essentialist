37cc68d90241caefcc88d1bdfaa7de9de7d1a894
Tim Phillips <timphillips@hey.com>
2023-05-24 11:15:33 -0700

    refactor(stats): use object in it.each

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
@@ -2,32 +2,32 @@ import { statsCalculator } from "./";
 
 describe(statsCalculator.name, () => {
   it.each([
-    [-8, [2, 4, 21, -8, 53, 40]],
-    [16, [29, 184, 21, 16, 53, 40]],
-  ])("knows that %i is the minimum value", (min, seq) => {
+    { min: -8, seq: [2, 4, 21, -8, 53, 40] },
+    { min: 16, seq: [29, 184, 21, 16, 53, 40] },
+  ])("knows that %i is the minimum value", ({ min, seq }) => {
     const response = statsCalculator(seq);
     expect(response.min).toBe(min);
   });
 
   it.each([
-    [53, [2, 4, 21, -8, 53, 40]],
-    [184, [29, 184, 21, 16, 53, 40]],
-  ])("knows that %i is the maximum value", (max, seq) => {
+    { max: 53, seq: [2, 4, 21, -8, 53, 40] },
+    { max: 184, seq: [29, 184, 21, 16, 53, 40] },
+  ])("knows that %i is the maximum value", ({ max, seq }) => {
     const response = statsCalculator(seq);
     expect(response.max).toBe(max);
   });
 
   it.each([
-    [6, [29, 184, 21, 16, 53, 40]],
-    [3, [29, 184, 21]],
-  ])("knows that there are %i elements", (elements, seq) => {
+    { elements: 6, seq: [29, 184, 21, 16, 53, 40] },
+    { elements: 3, seq: [29, 184, 21] },
+  ])("knows that there are %i elements", ({ elements, seq }) => {
     const response = statsCalculator(seq);
     expect(response.elements).toBe(elements);
   });
 
   it.each([
-    [18.666666666667, [2, 4, 21, -8, 53, 40]],
-    [-44.666666666667, [29, -184, 21]],
+    { avg: 18.666666666667, seq: [2, 4, 21, -8, 53, 40] },
+    { avg: -44.666666666667, seq: [29, -184, 21] },
   ])("knows that the average is 18.666666666667", () => {
     const seq = [2, 4, 21, -8, 53, 40];
     const response = statsCalculator(seq);
