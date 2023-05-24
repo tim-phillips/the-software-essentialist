76f544aa14dbdb5b6a507127778d1d5e70796d8e
Tim Phillips <timphillips@hey.com>
2023-05-24 11:22:26 -0700

    refactor(stats): consolidate tests

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
@@ -2,35 +2,42 @@ import { statsCalculator } from "./";
 
 describe(statsCalculator.name, () => {
   it.each([
-    { min: -8, seq: [2, 4, 21, -8, 53, 40] },
-    { min: 16, seq: [29, 184, 21, 16, 53, 40] },
-  ])("knows that %i is the minimum value", ({ min, seq }) => {
-    const response = statsCalculator(seq);
-    expect(response.min).toBe(min);
-  });
-
-  it.each([
-    { max: 53, seq: [2, 4, 21, -8, 53, 40] },
-    { max: 184, seq: [29, 184, 21, 16, 53, 40] },
-  ])("knows that %i is the maximum value", ({ max, seq }) => {
-    const response = statsCalculator(seq);
-    expect(response.max).toBe(max);
-  });
-
-  it.each([
-    { elements: 6, seq: [29, 184, 21, 16, 53, 40] },
-    { elements: 3, seq: [29, 184, 21] },
-  ])("knows that there are %i elements", ({ elements, seq }) => {
-    const response = statsCalculator(seq);
-    expect(response.elements).toBe(elements);
-  });
-
-  it.each([
-    { avg: 18.666666666667, seq: [2, 4, 21, -8, 53, 40] },
-    { avg: -44.666666666667, seq: [29, -184, 21] },
-  ])("knows that the average is 18.666666666667", () => {
-    const seq = [2, 4, 21, -8, 53, 40];
-    const response = statsCalculator(seq);
-    expect(response.avg).toBe(18.666666666667);
-  });
+    {
+      min: -8,
+      max: 53,
+      elements: 6,
+      avg: 18.666666666667,
+      seq: [2, 4, 21, -8, 53, 40],
+    },
+    {
+      min: 16,
+      max: 184,
+      elements: 6,
+      avg: 57.166666666667,
+      seq: [29, 184, 21, 16, 53, 40],
+    },
+    {
+      min: 21,
+      max: 184,
+      elements: 3,
+      avg: 78,
+      seq: [29, 184, 21],
+    },
+    {
+      min: -184,
+      max: 29,
+      elements: 3,
+      avg: -44.666666666667,
+      seq: [29, -184, 21],
+    },
+  ])(
+    "knows that min: $min, max: $max, elements: $elements, avg: $avg",
+    ({ min, max, elements, avg, seq }) => {
+      const response = statsCalculator(seq);
+      expect(response.min).toBe(min);
+      expect(response.max).toBe(max);
+      expect(response.elements).toBe(elements);
+      expect(response.avg).toBe(avg);
+    }
+  );
 });
