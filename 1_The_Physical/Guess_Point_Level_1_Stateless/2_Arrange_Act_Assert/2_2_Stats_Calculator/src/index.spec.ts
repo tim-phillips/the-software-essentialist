9398ba507218a555bfe4953cfaabdd0503f4a6fa
Tim Phillips <timphillips@hey.com>
2023-05-24 11:26:13 -0700

    refactor(stats): move seq to top of tests

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
@@ -3,36 +3,36 @@ import { statsCalculator } from "./";
 describe(statsCalculator.name, () => {
   it.each([
     {
+      seq: [2, 4, 21, -8, 53, 40],
       min: -8,
       max: 53,
       elements: 6,
       avg: 18.666666666667,
-      seq: [2, 4, 21, -8, 53, 40],
     },
     {
+      seq: [29, 184, 21, 16, 53, 40],
       min: 16,
       max: 184,
       elements: 6,
       avg: 57.166666666667,
-      seq: [29, 184, 21, 16, 53, 40],
     },
     {
+      seq: [29, 184, 21],
       min: 21,
       max: 184,
       elements: 3,
       avg: 78,
-      seq: [29, 184, 21],
     },
     {
+      seq: [29, -184, 21],
       min: -184,
       max: 29,
       elements: 3,
       avg: -44.666666666667,
-      seq: [29, -184, 21],
     },
   ])(
     "knows that min: $min, max: $max, elements: $elements, avg: $avg",
-    ({ min, max, elements, avg, seq }) => {
+    ({ seq, min, max, elements, avg }) => {
       const response = statsCalculator(seq);
       expect(response.min).toBe(min);
       expect(response.max).toBe(max);
