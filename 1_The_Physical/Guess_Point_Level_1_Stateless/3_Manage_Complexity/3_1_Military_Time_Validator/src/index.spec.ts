67976deac6584c69d366dcd5675ea463ab032526
Tim Phillips <timphillips@hey.com>
2023-05-25 08:35:25 -0700

    feat(military): fake it true

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -1,5 +1,12 @@
+import { MilitaryTimeValidator } from "./";
 
-describe('military time validator', () => {
+describe("military time validator", () => {
+  it('knows that "01:12 - 14:32" is a valid range', () => {
+    const range = "01:12 - 14:32";
+    const militaryTimeValidator = new MilitaryTimeValidator();
 
+    const res = militaryTimeValidator.validateRange(range);
 
-})
+    expect(res).toBe(true);
+  });
+});
