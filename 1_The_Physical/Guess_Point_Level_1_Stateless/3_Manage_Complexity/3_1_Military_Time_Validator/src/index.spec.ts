bbea718dfbc84c2340949e5eedbb35c7b856e1bc
Tim Phillips <timphillips@hey.com>
2023-05-25 19:42:33 -0700

    refactor(military): make method static

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -3,28 +3,19 @@ import { MilitaryTimeValidator } from "./";
 describe("military time validator", () => {
   it('knows that "01:12 - 14:32" is a valid range', () => {
     const range = "01:12 - 14:32";
-    const militaryTimeValidator = new MilitaryTimeValidator();
-
-    const res = militaryTimeValidator.validateRange(range);
-
+    const res = MilitaryTimeValidator.validateRange(range);
     expect(res).toBe(true);
   });
 
   it("knows that an empty string is not a valid range", () => {
     const range = "";
-    const militaryTimeValidator = new MilitaryTimeValidator();
-
-    const res = militaryTimeValidator.validateRange(range);
-
+    const res = MilitaryTimeValidator.validateRange(range);
     expect(res).toBe(false);
   });
 
   it('knows that "12:34" is not a valid range', () => {
     const range = "12:34";
-    const militaryTimeValidator = new MilitaryTimeValidator();
-
-    const res = militaryTimeValidator.validateRange(range);
-
+    const res = MilitaryTimeValidator.validateRange(range);
     expect(res).toBe(false);
   });
 });
