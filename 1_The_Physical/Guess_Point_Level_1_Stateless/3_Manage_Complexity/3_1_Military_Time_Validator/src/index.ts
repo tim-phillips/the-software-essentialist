2052405c0ae0784273f06145b9ed3ea29de6f372
Tim Phillips <timphillips@hey.com>
2023-05-25 19:53:00 -0700

    feat(military): check start for existence

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -2,8 +2,8 @@ export class MilitaryTimeValidator {
   static validateRange(range: string): boolean {
     if (!range.includes("-")) return false;
 
-    const [, end] = range.split(" - ");
-    if (!end) return false;
+    const [start, end] = range.split(" - ");
+    if (!start || !end) return false;
 
     return Boolean(range);
   }
