aa9fe06489135c74bd8da08ddc1539be77718a49
Tim Phillips <timphillips@hey.com>
2023-05-27 14:53:00 -0700

    feat(military): add negative start minute check

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -11,7 +11,7 @@ export class MilitaryTimeValidator {
     const [endHour, endMinute] = end.split(":");
     if (parseInt(startHour) >= 24 || parseInt(startHour) < 0) return false;
     if (parseInt(endHour) >= 24 || parseInt(endHour) < 0) return false;
-    if (parseInt(startMinute) >= 60) return false;
+    if (parseInt(startMinute) >= 60 || parseInt(startMinute) < 0) return false;
     if (parseInt(endMinute) >= 60) return false;
 
     return Boolean(range);
