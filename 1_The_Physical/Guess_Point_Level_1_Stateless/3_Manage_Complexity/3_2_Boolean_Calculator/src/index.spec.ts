0d569ed75167764d7d1af480d608e47ac1b4b5fb
Tim Phillips <timphillips@hey.com>
2023-06-12 11:14:59 -0700

    test(boolean): add precedence tests

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
@@ -1,12 +1,16 @@
 import { BooleanCalculator } from "./";
 
 describe(BooleanCalculator.name, () => {
-  it.each(["TRUE", "NOT FALSE", "TRUE AND TRUE", "TRUE OR FALSE"])(
-    "'%s' is `true`",
-    (expression) => {
-      expect(BooleanCalculator.evaluateExpression(expression)).toEqual(true);
-    }
-  );
+  it.each([
+    "TRUE",
+    "NOT FALSE",
+    "TRUE AND TRUE",
+    "TRUE OR FALSE",
+    "TRUE OR TRUE OR TRUE AND FALSE",
+    "TRUE OR FALSE AND NOT FALSE",
+  ])("'%s' is `true`", (expression) => {
+    expect(BooleanCalculator.evaluateExpression(expression)).toEqual(true);
+  });
 
   it.each(["FALSE", "NOT TRUE", "TRUE AND FALSE", "FALSE OR FALSE"])(
     "'%s' is `false`",
